1b7c694e74564e2f0706d10108da0fca66e8db18
HosamAlden <hosam98j@gmail.com>
2023-03-31 01:38:35 +0200

    edit upload profile img api url

--- src/apis/filesUplaod.ts
+++ src/apis/filesUplaod.ts
@@ -1,10 +1,13 @@
+// get tha api url from .env file
+const api_url = process.env.REACT_APP_API_URL!;
+
 export const uploadProfileImg = (profile_img: any): Promise<string> => {
     const config = {
         method: "POST",
         body: profile_img,
     };
     return new Promise(async (resolve, reject) => {
-        const req = await fetch("http://localhost:2000/files/upload_profile_img", config);
+        const req = await fetch(`${api_url}/files/upload_profile_img`, config);
         // check for api response status
         if (req.status === 200) {
             const reqData = await req.json() as {url: string};
